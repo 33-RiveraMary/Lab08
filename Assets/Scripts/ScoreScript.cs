d36f2200cb8fba724c59271c2a58af82dc0c8c33
33-RiveraMary <114891412+33-RiveraMary@users.noreply.github.com>
2022-10-26 11:17:05 +0800

    Added score update

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    //increase score
+    private int score;
+    public Text scoreTxt;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            //destroy obstacle + increase score
+            Destroy(other.gameObject);
+
+            score++;
+            scoreTxt.text = "Score: " + score;
+        }
+    }
+}
